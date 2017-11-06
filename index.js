0a5edcf28e3f9e65f5542b07e93f6cf8d59895c5
PJ Frias <peejfrias@gmail.com>
2017-11-06 14:55:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+const driver = {}
+debugger;
+console.log('test1')
+function updatedriverWithKeyAndValue(driver, key, value) {
+  debugger;
+  const newObj = Object.assign({}, driver, { key : value })
+  debugger;
+  console.log(newObj)
+  return newObj
+}
+console.log('test2')
