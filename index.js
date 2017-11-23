3ce2d3330187fab7c14acde892fd52dd710ee46b
John Sillings <johnsillings@gmail.com>
2017-11-23 00:30:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+let driver = {
+  name: 'Sam'
+};
+
+function updateDriverWithKeyAndValue () {
+  let newDriver = {...driver};
+  newDriver.address = "11 Broadway";
+  return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue () {
+  driver.address = "12 Broadway";
+  return driver;
+}
+
+function deleteFromDriverByKey () {
+  let deletedNewDriver = {...driver};
+  delete deletedNewDriver.name;
+  return deletedNewDriver;
+}
+
+function destructivelyDeleteFromDriverByKey () {
+  delete driver.name;
+  return driver;
+}
